b9ee285b72faf94f7242237867c702d53d2bb6b6
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-27 13:19:11 -0500

    feat: create custom response, part 2

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import jwt from 'jsonwebtoken'
 
 export default class MyRouter {
     constructor() {
@@ -12,8 +13,17 @@ export default class MyRouter {
         return this.router
     }
 
-    get(path, ...callbacks) {
-        this.router.get(path, this.generateCustomResponses, this.applyCallbacks(callbacks))
+    get(path, policies, ...callbacks) {
+        this.router.get(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks))
+    }
+    post(path, policies, ...callbacks) {
+        this.router.post(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks))
+    }
+    put(path, policies, ...callbacks) {
+        this.router.put(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks))
+    }
+    delete(path, policies, ...callbacks) {
+        this.router.delete(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks))
     }
 
     applyCallbacks(callbacks) {
@@ -28,6 +38,29 @@ export default class MyRouter {
 
     generateCustomResponses(req, res, next) {
        res.sendSuccess = payload => res.send({ status: 'success', payload }) 
+       res.sendServerError = error => res.status(500).send({ status: 'error', error }) 
+       res.sendUserError = error => res.status(400).send({ status: 'error', error })
+       res.sendNoAuthenticatedError = error => res.status(401).send({ status: 'error', error })
+       res.sendNoAuthorizatedError = error => res.status(403).send({ status: 'error', error })
        next()
+    }
+
+    handlePolicies = policies => (req, res, next) => {
+        if (policies.includes('PUBLIC')) return next()
+        if (policies.length > 0) {
+            const authHeaders = req.headers.authorization
+
+            if (!authHeaders) return res.sendNoAuthenticatedError('Unauthenticated')
+
+            const tokenArray = authHeaders.split(' ')
+            const token = (tokenArray.length > 1) ? tokenArray[1] : tokenArray[0]
+
+            const user = jwt.verify(token, 'secret')
+
+            if (!policies.includes(user.role.toUpperCase())) {
+                return res.sendNoAuthorizatedError('Unauthorizated')
+            }
+        }
+        next()
     }
 }
